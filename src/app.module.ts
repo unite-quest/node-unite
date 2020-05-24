5825743905ab41ff86f2ab634d3b0bd9e16ab7b0
Gabriel Nishimura <gabrieltnishimura@gmail.com>
2020-05-24 10:51:34 -0300

    feat(config): added dependency to use local .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordingModule } from './recording/recording.module';
 
 @Module({
-  imports: [RecordingModule],
+  imports: [
+    ConfigModule.forRoot(),
+    RecordingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
