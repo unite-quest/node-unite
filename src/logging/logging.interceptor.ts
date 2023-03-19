000fe867160d60ed712d36c4941b2c1872d7bd47
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2023-03-19 19:51:03 -0300

    fix(guest): guest user doesn't crash on first db request, sentry version updated

--- src/logging/logging.interceptor.ts
+++ src/logging/logging.interceptor.ts
@@ -1,23 +1,20 @@
 import {
-  CallHandler, ExecutionContext,
+  CallHandler,
+  ExecutionContext,
   Injectable,
-  NestInterceptor
+  NestInterceptor,
 } from '@nestjs/common';
-import * as Sentry from '@sentry/minimal';
+import * as Sentry from '@sentry/node';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(
-        tap(null, (exception) => {
-          Sentry.captureException(exception);
-        }),
-      );
+    return next.handle().pipe(
+      tap(null, exception => {
+        Sentry.captureException(exception);
+      }),
+    );
   }
-
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as Sentry from '@sentry/node';
+import '@sentry/tracing';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './logging/logging.interceptor';
 const requestContext = require('request-context'); // tslint:disable-line
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  Sentry.init({ dsn: 'https://ffd01336fa654be3922f48a1e3bacf1d@o433447.ingest.sentry.io/5392260' });
+  Sentry.init({
+    dsn:
+      'https://ffd01336fa654be3922f48a1e3bacf1d@o433447.ingest.sentry.io/5392260',
+    tracesSampleRate: 1.0,
+  });
   app.use(requestContext.middleware('request'));
-  app.useGlobalInterceptors(new LoggingInterceptor())
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(process.env.PORT || 80);
 }
 bootstrap();
-
-
